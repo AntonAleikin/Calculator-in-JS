afe5f49a24321eeeff26926a83df4c2ff96073a1
Anton <aleikinaa98@gmail.com>
2020-08-02 14:23:28 +0300

    Multi-operators and arguments calculation is ready

--- js/calculator.js
+++ js/calculator.js
@@ -24,11 +24,13 @@ let numStorage = {
 // Объект для хранения результатов действий операторов
 let results = {
     plus: "",
+    multiply: ""
 };
 
 // Тут равно проверяет какой был выполнен оператор
 let id = {
     plus: "",
+    multiply: ""
 };
 
 
@@ -42,11 +44,13 @@ function argument1 (e) {
             numStorage.arg1.push(numbers.num1);
             numStorage.argNum1 = Number(numStorage.arg1.join(""));
             console.log(numStorage.argNum1);
-        } else if (btn.classList.contains("num_2")) {
+        } 
+        if (btn.classList.contains("num_2")) {
             numStorage.arg1.push(numbers.num2);
             numStorage.argNum1 = Number(numStorage.arg1.join(""));
             console.log(numStorage.argNum1);
-        } else if (btn.classList.contains("num_3")) {
+        } 
+        if (btn.classList.contains("num_3")) {
             numStorage.arg1.push(numbers.num3);
             numStorage.argNum1 = Number(numStorage.arg1.join(""));
             console.log(numStorage.argNum1);
@@ -64,11 +68,13 @@ function argument2 (e) {
             numStorage.arg2.push(numbers.num1);
             numStorage.argNum2 = Number(numStorage.arg2.join(""));
             console.log(numStorage.argNum2);
-        } else if (btn.classList.contains("num_2")) {
+        }
+        if (btn.classList.contains("num_2")) {
             numStorage.arg2.push(numbers.num2);
             numStorage.argNum2 = Number(numStorage.arg2.join(""));
             console.log(numStorage.argNum2);
-        } else if (btn.classList.contains("num_3")) {
+        }
+        if (btn.classList.contains("num_3")) {
             numStorage.arg2.push(numbers.num3);
             numStorage.argNum2 = Number(numStorage.arg2.join(""));
             console.log(numStorage.argNum2);
@@ -76,31 +82,56 @@ function argument2 (e) {
     }
 }
 
+
+// Реализация цекочки из сразу нескольких разных операций 
+function multiOperations () {
+    
+    // Создаем 3й аргумент как очищеный 2й, а 1й = 1й+2й 
+    if (numStorage.argNum2 !="") { 
+        if (id.plus == 1) { 
+            numStorage.argNum1 = numStorage.argNum1 + numStorage.argNum2;
+            id.plus = "";
+        } else if (id.multiply == 1) {
+            numStorage.argNum1 = numStorage.argNum1 * numStorage.argNum2;
+            id.multiply = "";
+        }
+        numStorage.arg2 = [];
+    } else {
+        section[0].removeEventListener("click", argument1);
+        section[0].addEventListener("click", argument2);
+    }
+}
+
 // Ф-ция операторов
-function operators (e) {
+function operations (e) {
     let btn = e.target;
     if (btn && btn.classList.contains("section_item")) {
 
         // Плюс
         if (btn.classList.contains("plus")) {
+            multiOperations();
+            id.plus = 1;
+        }
 
-            if (numStorage.argNum2 !="") { // Создаем 3й аргумент как очищеный 2й, а 1й = 1й+2й 
-                numStorage.argNum1 = numStorage.argNum1 + numStorage.argNum2;
-                numStorage.arg2 = [];
-                id.plus = 1;
-            } else {
-                section[0].removeEventListener("click", argument1);
-                section[0].addEventListener("click", argument2);
-                id.plus = 1;
-            }
+        // Умножение
+        if (btn.classList.contains("multiply")) {
+            multiOperations();
+            id.multiply = 1;
         }
 
         // Равно
         if (btn.classList.contains("equate")) {
+
+            // Плюс
             if (id.plus !="") { // Проверяем айди оператора (был ли он выполнен)
                 results.plus = numStorage.argNum1 + numStorage.argNum2;
                 console.log(results.plus);
-                id.plus = ""; 
+            }
+
+            // Умножение
+            if (id.multiply !="") { 
+                results.multiply = numStorage.argNum1 * numStorage.argNum2;
+                console.log(results.multiply); 
             }
         }
     }
@@ -108,7 +139,7 @@ function operators (e) {
 
 // Делигируем события на секции с кнопками
 section[0].addEventListener("click", argument1);
-section[1].addEventListener("click", operators);
+section[1].addEventListener("click", operations);
 
 
 
